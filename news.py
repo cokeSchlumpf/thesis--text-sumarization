68876b4611d97185bc89831b79174226f4d4e9a5
Michael Wellner <michaelwellner@kpmg.com>
2020-12-31 12:25:38 +0100

    some restructuring

--- news.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-DATA_PATH = '/Users/michaelwellner/Workspaces/thesis--news-crawler/export'
-METADATA_FILE = f"{DATA_PATH}/_items.csv"
-
-def load_text(row, data_path = DATA_PATH):
-    hash = row['hash']
-    with open(f"{data_path}/{hash}.content.txt", 'r') as file:
-        return file.read()
